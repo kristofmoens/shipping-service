d866b5ef077e68caabb1697f9427358643e66a44
Kristof Moens <kristof.moens@be.ibm.com>
2019-05-22 17:46:36 +0200

    Adding logging

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 // src/app.js
 const express = require('express')
 const app = express()
+var log = require('cf-nodejs-logging-support');
 const ShippingController = require('../src/controllers/shipping-controller')
+
+// Set the minimum logging level (Levels: error, warn, info, verbose, debug, silly)
+log.setLoggingLevel("info");
+
+// Bind to express app
+app.use(log.logNetwork);
 
 app.get('/*shipping', (request, response) => {
   let ctrl = new ShippingController()
+  // Set the minimum logging level (Levels: error, warn, info, verbose, debug, silly)
+    req.logger.info("Hello watcher, i'm watching you!");
 
   ctrl
     .getItemShipping({id: request.query.itemId, type: request.query.type})
@@ -17,5 +26,6 @@ app.get('/*shipping', (request, response) => {
 
 })
 
+log.info("Server is listening on port %d", 3000);
 let PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`ShippingService is listening on port ${PORT}`))
